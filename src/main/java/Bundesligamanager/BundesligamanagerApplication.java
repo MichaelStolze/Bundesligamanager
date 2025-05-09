8d8613ff59dc4c610e99a6306e8fc583d1eab5cf
SHADOW-HOUOJ56D\micha <michael.stolze@stolze-concept.de>
2025-05-09 17:49:30 +0200

    Minimum runable version

---
+++ src/main/java/Bundesligamanager/BundesligamanagerApplication.java
@@ -0,0 +1,14 @@
+package Bundesligamanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+BundesligamanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BundesligamanagerApplication.class, args);
+	}
+
+}
